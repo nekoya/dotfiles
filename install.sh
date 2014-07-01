251ce1c0db6aa75990df4b1b9dc80969a7ebffea
Ryo Miyake <ryo.studiom@gmail.com>
2014-07-01 12:23:54 +0900

    Follow neobundle install

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ copy mint.rb
 
 # vim
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-copy .vim
+cp -a ${SELF_DIR}/$1 ~
 if [ ! -e ~/.vimrc ]; then
     ln -s ~/.vim/.vimrc ~
 fi
